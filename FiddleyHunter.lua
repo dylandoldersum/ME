14d90464649a8fc02003824a3c04387a00ec41f7
Fiddle <dylandoldersum@outlook.com>
2024-02-10 23:47:33 +0100

    Update FiddleyHunter.lua

--- FiddleyHunter.lua
+++ FiddleyHunter.lua
@@ -4,7 +4,8 @@ COLORS = require("colors")
 
 local pickedWhirl = {};
 local whirlieNPCS = {28726, 28720, 28722, 28723, 28724, 28725, 28719}
-local roseStages = {122504, 122505, 122506, 122507 }
+local flowerStages = {  }
+local basketId = 0
 local threatScarab = 28671
 local unhandledFrito = 28665;
 local flowerBasket = false;
@@ -13,17 +14,49 @@ startTime, afk = os.time(), os.time()
 local Cselect =
     API.ScriptDialogWindow2(
     "Whirligig",
-    {"Catch Whirligigs", "Cultivate rose"},
+    {"Catch Whirligigs", "Cultivate Flowers"},
     "Start",
     "Close"
 ).Name
 
+local function getAttackState()
+    local whirlStackVB = API.VB_FindPSett(10338).state 
+    local whirlStack = whirlStackVB >> 20
+    return whirlStack
+end
 
+local function getBasketQuantity()
+    local flowerBasketVB = API.VB_FindPSett(10330).state
+    local flowerQuant64 = flowerBasketVB >> 18 & 0xfff
+    return flowerQuant64 / 64
+end
 
 if Cselect == "Catch Whirligigs" then
-    local Ccheck = API.ScriptDialogWindow2("Flower basket?", {"Yes", "No"}, "Start", "Close").Name
-    if Ccheck == "Yes" then
+    local Ccheck = API.ScriptDialogWindow2("Flower basket?", {"Roses", "Iris", "Hydrangea", "None"}, "Start", "Close").Name
+    if Ccheck == "Roses" then
         flowerBasket = true
+        basketId = 122495
+    end
+    if Ccheck == "Iris" then
+        flowerBasket = true
+        basketId = 122496
+    end
+    if Ccheck == "Hydrangea" then
+        flowerBasket = true
+        basketId = 122497
+    end
+end
+
+if Cselect == "Cultivate Flowers" then
+    local pickedFlower = API.ScriptDialogWindow2("Flower type", {"Roses", "Iris", "Hydrangea"}, "Start", "Close").Name
+    if pickedFlower == "Roses" then
+        flowerStages = {122504, 122505, 122506, 122507 }
+    end
+    if pickedFlower == "Iris" then
+        flowerStages = {122508, 122509, 122510, 122511 }
+    end
+    if pickedFlower == "Hydrangea" then
+        flowerStages = {122512, 122513, 122514, 122515 }
     end
 end
 
@@ -37,12 +70,12 @@ local function idleCheck()
     end
 end
 
-function findNpcOrObject(npcid, distance, objType)
+local function findNpcOrObject(npcid, distance, objType)
     local distance = distance or 20
     return #API.GetAllObjArrayInteract({ npcid }, distance, objType) > 0
 end
 
-function run_to_tile(x, y, z)
+local function run_to_tile(x, y, z)
     local tile = WPOINT.new(x, y, z)
     API.DoAction_Tile(tile)
     while API.Read_LoopyLoop() and API.Math_DistanceW(API.PlayerCoord(), tile) > 5 do
@@ -50,7 +83,7 @@ function run_to_tile(x, y, z)
     end
 end
 
-function randomWalk()
+local function randomWalk()
     local randomW = math.random(1,40)
     if randomW == 5 then
         print ("Random walking")
@@ -58,33 +91,21 @@ function randomWalk()
     end
 end
 
-function fillRoses()
+
+local function fillRoses()
     print ("Basket quantity: ", getBasketQuantity())
-    if getBasketQuantity() <= 5 then
+    if getBasketQuantity() <= 9 then
         print ("Time to refill basket")
         UTILS.randomSleep(5000)
         print ("5 secs sleep")
-        API.DoAction_Object1(0x29,240,{ 122495 },50)
+        API.DoAction_Object1(0x29,240,{ basketId },50)
         UTILS.randomSleep(1500)
         API.WaitUntilMovingEnds()
         print("Roses refilled")
     end
 end
 
-function cultivateRose()
-    if API.GetPlayerAnimation_(API.GetLocalPlayerName()) == -1 then
-        print ("Not doing anything..")
-        for i in ipairs(roseStages) do
-            if findNpcOrObject(roseStages[i], 3, 0) then
-                print("Found current rose stage!", roseStages[i])
-                API.DoAction_Object1(0x29,0,{ roseStages[i] },50)
-            end
-        end
-        checkForThreats()
-    end
-end
-
-function checkForThreats()
+local function checkForThreats()
     print("Looking for threats")
     if findNpcOrObject(threatScarab, 10, 1) then
         print("Pasty Scarab found! Shoo away")
@@ -95,22 +116,47 @@ function checkForThreats()
     UTILS.randomSleep(1500)
 end
 
-function getFritoState()
+local function cultivateFlowers()
+    print ("Check if still busy..")
+    if not API.CheckAnim(100) then
+        for i in ipairs(flowerStages) do
+            if findNpcOrObject(flowerStages[i], 3, 0) then
+                print("Found current rose stage!", flowerStages[i])
+                API.DoAction_Object1(0x29,0,{ flowerStages[i] },50)
+            end
+        end
+    end
+    checkForThreats()
+end
+
+local function getFritoState()
     return (API.VB_FindPSett(10339).state)
 end
 
-function getAttackState()
-    local whirlStackVB = API.VB_FindPSett(10338).state 
-    return whirlStackVB
+
+
+local function catchWhirls()
+    for i in ipairs(whirlieNPCS) do
+        if API.Buffbar_GetIDstatus(52770).conv_text >=5 then
+            UTILS.randomSleep(2000);
+            print("Idle for 2 seconds, cuz stack is above 5")
+        end
+
+        API.DoAction_NPC(0x29,1488,{ whirlieNPCS[i] }, 50)
+        UTILS.randomSleep(1000)
+        if flowerBasket then
+            if API.InvItemcount_1(52808) > 0 then
+                fillRoses()
+            else
+                print("No more flowers")
+                break
+            end
+        end
+        randomWalk()
+    end
 end
 
-function getBasketQuantity()
-    local flowerBasketVB = API.VB_FindPSett(10330).state
-    local flowerQuant64 = flowerBasketVB >> 18 & 0xfff
-    return flowerQuant64 / 64
-end
-
-function handleFrito()
+local function handleFrito()
     if getFritoState() > 1 then
         catchWhirls()
     else
@@ -120,27 +166,18 @@ function handleFrito()
     end  
 end
 
-function catchWhirls()
-    for i in ipairs(whirlieNPCS) do
-        if API.Buffbar_GetIDstatus(52770).conv_text >=5 then
-            UTILS.randomSleep(2000);
-            print("Idle for 2 seconds, cuz stack is above 5")
-        end
-        API.DoAction_NPC(0x29,1488,{ whirlieNPCS[i] }, 50)
-        UTILS.randomSleep(1500)
-        if flowerBasket then
-            fillRoses()
-        end
-        randomWalk()
-    end
-end
+
 
 while API.Read_LoopyLoop() do
     idleCheck()
+    if flowerBasket and API.InvItemcount_1(52808) < 1 then
+        print("No more flowers")
+        break
+    end
     if not string.match(Cselect, "Cultivate") then    
         handleFrito()
     else
-        cultivateRose()
+        cultivateFlowers()
     end
     API.SetDrawTrackedSkills(true)
 end
